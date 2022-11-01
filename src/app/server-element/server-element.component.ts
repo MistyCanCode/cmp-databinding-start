8864d02fd837569d7a54706133b750fad71b9d72
Misty Tomlin (OSV) <misty.tomlin.osv@fedex.com>
2022-11-01 13:32:31 -0500

    worked through lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //none, native, emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit , OnChanges, DoCheck{
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called!');
+   }
+
+ngOnChanges(changes: SimpleChanges){
+  console.log('ngOnChanges called!');
+  console.log(changes);
+}
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck(): void {
+      console.log('ngDoCheck called!');
   }
 
 }
